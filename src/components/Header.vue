e77167e6a1cfc6664ffddbcc9c3496b0f0e4f0c6
eyder <eydertinoco@outlook.com>
2022-03-11 13:45:16 -0300

    Criando Header;

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li>
+      <a href="#">Home</a>
+      <a href="#">Projeto</a>
+      <a href="#">Contato</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
