14650eb9a5c5a5c93787d6036cfbe9c84a2a0358
Eyder <eydertinoco@outlook.com>
2022-03-13 19:35:55 -0300

    Adição do Dashboard;

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar Pedidos:</h1>
+    <Dashboard/>
   </div>
 </template>
 
 <script>
+import Dashboard from "@/components/Dashboard";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Pedidos',
+  components: {Dashboard},
 }
-
 </script>
 
 <style scoped>
